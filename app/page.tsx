2e188bb57df0251894d988b4490de6d70028b6bf
Ode Ardika <104220059+odeardika@users.noreply.github.com>
2025-01-10 21:40:46 +0800

    feat: create hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Header from "@/components/Header/Header";
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
-
   return (
     <div className="">
       <Header />
+      <Hero />
+
     </div>
   );
 }
---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="Hero Section relative h-screen">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src="/video/background.mov"
+        autoPlay
+        muted
+        loop
+        playsInline
+      ></video>
+
+      {/* Overlay Gelap */}
+      <div className="absolute top-0 left-0 w-full h-full bg-black/60"></div>
+
+      {/* Content di atas video */}
+      <div className="relative z-10 flex flex-col items-center justify-center h-full md:gap-6 gap-4">
+        <h1 className="text-white text-4xl sm:text-5xl md:text-6xl font-bold">
+          Kubu Village
+        </h1>
+        <h2 className="text-white text-base sm:text-2xl md:text-3xl">
+          Uncover the Hidden Wonders of Kubu Village
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
