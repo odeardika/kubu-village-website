4d897951dafd584ebb722424a0d89c73408cc7ec
Ode Ardika <104220059+odeardika@users.noreply.github.com>
2025-01-11 10:42:59 +0800

    fix: fix contact us button

--- components/Button/ContactUsButton.tsx
+++ components/Button/ContactUsButton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ContactUsButton({ href }: { href: string }) {
   return (
     <button className="hidden md:block border-2 px-4 py-2 border-primary-800 text-primary-800 rounded-md font-semibold">
-      Contact Us
+      <a href={href}>Contact Us</a>
     </button>
   );
 }
